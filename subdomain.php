8682ce83f880015f1ca3b9219e92ba145b95aefc
Galeh Rizky <39430605+JiwaTerlelap@users.noreply.github.com>
2019-03-29 19:55:11 -0700

    Update subdomain.php
    add main function

--- subdomain.php
+++ subdomain.php
@@ -6,33 +6,42 @@
  * Open Result subdomain.txt !
  * Usage :  Usage : php subdomain site.com
  */
-error_reporting(0);
 class Scan{
-	function ngecek($url){
+	public function ngecek($url){
 			$ch = curl_init();
 			$options = [
               CURLOPT_URL => "https://findsubdomains.com/subdomains-of/$url",
               CURLOPT_RETURNTRANSFER => TRUE,
               CURLOPT_USERAGENT => "Mozilla/5.0 (Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0",
-              CURLOPT_SSL_VERIFYHOST  => FALSE,
-              CURLOPT_SSL_VERIFYPEER  => FALSE
+              CURLOPT_SSL_VERIFYHOST  => false,
+              CURLOPT_SSL_VERIFYPEER  => false
 			];
 			curl_setopt_array($ch, $options);
-			$aksi = curl_exec($ch);
-			// print_r($aksi);
-			preg_match_all("/<div class=\"domains js-domain-name\">([^`]*?)<\/div>/",$aksi, $data);
-			  foreach (str_replace(" ", "", $data[1]) as $key) {
-			  	    echo "[+] Di Temukan -> {$key}\n";
-			  	    $file = fopen("subdomain.txt", "a");
-			  	            fwrite($file, $key. "\r\n");
-			  	            fclose($file);
-			  }
+			return curl_exec($ch);
 	}
+	public function main($url){
+		$data = $this->ngecek($url);
+		if(preg_match_all("/<div class=\"domains js-domain-name\">([^`]*?)<\/div>/", $data, $result)){
+			foreach (str_replace(" ", "", $result[1]) as $key => $value) {
+				echo "[+] Subdomain : " . $value . "\r\n";
+				$filename = date('ymdhis') . " - subdomain.txt";
+				$this->save($filename, $value);
+			}
+		}
+		else
+		  echo "[-] Gagal menemukan subdomain !";
+			
+	}
+	public function save($filename, $result){
+		 $file = fopen($filename, "a");
+		         fwrite($file, $result . "\r\n");
+		         fclose($file);
+	}
+  
 }
-$dork = new Scan();
-$get = $argv[1];
- if($get){
-     $dork->ngecek($get);
+$scan = new Scan();
+ if($argv[1]){
+     $scan->main($argv[1]);
    }else{
 	  echo "Usage php subdomain.php site.com";
 }
